b8a68e30edb6170dfc77c8c7c6a36bb2c93ad994
Grace Kuo <gkuo.github@gmail.com>
2021-03-07 20:19:24 -0700

    header and nav bar created

---
+++ client/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>(React) Search for Books Below!</h1>
+      <h3>Search for Any Book and Save Any Books of Interest</h3>
+    </div>
+  );
+}
+
+export default Jumbotron;
