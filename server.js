26dd7a85d0d7b8d27257ede8feba5ee29046b9a5
Grace Kuo <gkuo.github@gmail.com>
2021-03-07 21:58:11 -0700

    deploying

--- server.js
+++ server.js
@@ -12,11 +12,17 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/limitless-retreat-68581',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
 
-// Define API routes here
+app.use("/api", apiRoutes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
